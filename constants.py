a585b6828782028d2210a18af4c2763ca7ab52db
Lenkey Nicolas <nicolas.lenkey@gmail.com>
2021-04-15 21:19:33 +0200

    Feat: without arguments the program prints the usage information

---
+++ constants.py
@@ -0,0 +1,15 @@
+
+
+FILE_NAME = "todo-list.txt"
+
+# Usage information about the app, with the correct formatting
+USAGE_INFORMATION = """
+Command Line Todo application
+=============================
+
+Command line arguments:
+    -l Lists all the tasks
+    -a Adds a new task
+    -r Removes a task
+    -c Completes a task
+ """
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import sys
+import constants
+
+
+# The main function only responsible for the control flow based on the arguments
+def main():
+    # This is the case where no args were given
+    if (len(sys.argv) == 1):
+        print(constants.USAGE_INFORMATION)
+
+if __name__ == "__main__":
+    main()
