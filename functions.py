eff1ef75ab0bcaa5a5927f903830b1d70e10be21
Lenkey Nicolas <nicolas.lenkey@gmail.com>
2021-04-16 18:25:17 +0200

    Feat: added remove and check, removed comments

--- functions.py
+++ functions.py
@@ -1,9 +1,54 @@
 import constants
 
 
-# Creates generic error message based on the method and on
-# the type of the error. Always returns a True if it was called
-# The possible errorType values are defined in the constans file
+def is_integer(variable):
+    try:
+        int(variable)
+        return True
+    except:
+        return False
+
+
+def row_parser(row, indicator_symbol):
+    updated_row = ""
+    indicator = False
+    for char in row:
+        if char == indicator_symbol:
+            indicator = True
+        if indicator:
+            updated_row = updated_row + char
+    return updated_row
+
+
+def method_name_converter(method):
+    if method == "-l":
+        return "list"
+    if method == "-a":
+        return "add"
+    if method == "-r":
+        return "remove"
+    if method == "-c":
+        return "check"
+
+
+def rebuild_list(given_list, method, chosen_row):
+    counter = 1
+    result_list = ""
+    for row in given_list:
+        if method == "remove":
+            if counter < chosen_row:
+                result_list = result_list + row
+            elif counter > chosen_row:
+                result_list = result_list + "{} {}".format(counter - 1, row_parser(row, "-"))
+        elif method == "check":
+            if counter == chosen_row:
+                result_list = result_list + "{} - [x{}".format(counter, row_parser(row, "]"))
+            else:
+                result_list = result_list + row
+        counter = counter + 1
+    return result_list
+
+
 def create_error(method, error_type):
     if error_type == 1:
         print("\nUnable to {}: index is out of bound\n".format(method))
@@ -21,9 +66,7 @@ def create_error(method, error_type):
     return True
 
 
-# Prints the content of the file. If the files doesn't exist, creates one
 def read_from_file(file_name):
-    # The following is necessary, it creates the file if it now exist yet
     f = open("todo-list.txt", "a")
     f.close()
 
@@ -36,9 +79,18 @@ def read_from_file(file_name):
         print(f.read())
 
 
-# Writes at the end of the file
 def append_to_file(file_name, task):
     current_row_number = len(open(file_name).readlines())
     f = open(file_name, "a")
     f.write("{} - [ ] {}\n".format(current_row_number + 1, task))
     f.close()
+
+
+def rewrite_file(file_name, chosen_row, method):
+    current_rows = open(file_name).readlines()
+    if len(current_rows) < chosen_row or chosen_row < 1:
+        create_error(method, constants.Error.OUT_OF_BOUND)
+    else:
+        f = open(file_name, "w")
+        f.write(rebuild_list(current_rows, method, chosen_row))
+        f.close()
--- main.py
+++ main.py
@@ -3,24 +3,25 @@ import constants
 import functions
 
 
-# The main function only responsible for the control flow based on the arguments
 def main():
-    # This is the case where no args were given
     if len(sys.argv) == 1:
         print(constants.USAGE_INFORMATION)
 
-    # The only case where there is only one arg is the "-l" list function
-    # at every other methods this case is just used for error handling
     elif len(sys.argv) == 2:
 
         if sys.argv[1] == "-l":
             functions.read_from_file(constants.FILE_NAME)
 
-    # Handling the cases where the number of arguments is 3
     elif len(sys.argv) == 3:
 
         if sys.argv[1] == "-a":
             functions.append_to_file(constants.FILE_NAME, sys.argv[2])
+
+        elif (sys.argv[1] == "-r") or (sys.argv[1] == "-c"):
+            if functions.is_integer(sys.argv[2]):
+                functions.rewrite_file(constants.FILE_NAME, int(sys.argv[2]), functions.method_name_converter(sys.argv[1]))
+            else:
+                functions.create_error(functions.method_name_converter(sys.argv[1]), constants.Error.INDEX_NOT_A_NUMBER)
 
 
 if __name__ == "__main__":
